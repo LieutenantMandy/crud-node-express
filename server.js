089d49575076008c447cbc72318c18158a03d6a9
LieutenantMandy <your_email>
2023-04-11 09:37:48 +0530

    Proper Formatting

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express(); app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const UserRoute = require('./routes/User') 
-app.use('/user',UserRoute)
+const UserRoute = require("./routes/User");
+app.use("/user", UserRoute);
 
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.url, {
-useNewUrlParser: true }).then(() => {
-console.log("Databse Connected Successfully!!"); }).catch(err => { console.log('Could not connect to the database', err); process.exit(); });
-app.get('/', (req, res) => { res.json({"message": "Hello Crud Node Express"}); });
-app.listen(3000, () => { console.log("Server is listening on port 3000"); });
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Databse Connected Successfully!!");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database", err);
+    process.exit();
+  });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Crud Node Express" });
+});
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
